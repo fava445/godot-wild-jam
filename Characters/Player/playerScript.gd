5f149777abac2bf7977145dbba9b6fd82eae0e1e
TiberiusGracchus <philipsteck2209@gmail.com>
2024-02-10 16:47:54 +0100

    Updated Movement

--- Characters/Player/playerScript.gd
+++ Characters/Player/playerScript.gd
@@ -11,29 +11,14 @@ func _process(delta):
 
 
 func playerMovement():
-	self.position.y += SpeedY
-	self.position.x += SpeedX
-	if Input.is_action_pressed("Down") and SpeedY <= MAX_SPEED:
-		SpeedY += SPEED_GAIN
-	if Input.is_action_pressed("Up") and SpeedY >= MIN_SPEED:
-		SpeedY -= SPEED_GAIN
-	if Input.is_action_pressed("Right") and SpeedX <= MAX_SPEED:
-		SpeedX += SPEED_GAIN
-	if Input.is_action_pressed("Left") and SpeedX <= MAX_SPEED:
-		SpeedX -= SPEED_GAIN
-	if SpeedX <= MIN_SPEED or SpeedY <= MIN_SPEED :
-		SpeedX = -30
-		SpeedY = -30
-	if SpeedX >= MAX_SPEED or SpeedY >= MAX_SPEED:
-		SpeedX = 30
-		SpeedY = 30
-	if not SpeedY == 0:
-		if SpeedY > 0:
-			SpeedY += SPEED_LOSS
-		if SpeedY < 0:
-			SpeedY -= SPEED_LOSS
-	if not SpeedX == 0:
-		if SpeedX > 0:
-			SpeedX += SPEED_LOSS
-		if SpeedX < 0:
-			SpeedX -= SPEED_LOSS
+	if Input.is_action_pressed("Down"):
+		self.position.y += 2
+	if Input.is_action_pressed("Up"):
+		self.position.y -= 2
+	if Input.is_action_pressed("Left"):
+		if not Input.is_action_pressed("Right"):
+			self.position.x -= 2
+	if Input.is_action_pressed("Right"):
+		if not Input.is_action_pressed("Left"):	
+			self.position.x += 2
+	move_and_slide()
